858b48c2e42b35a5e3c2d75baf4f74b4bb5c1617
mateusz <adamus.mat@gmail.com>
2020-02-04 22:26:32 +0100

    Initial commit

---
+++ src/main/java/com/mateusz/adamus/zusservice/ZusserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mateusz.adamus.zusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZusserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZusserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateusz/adamus/zusservice/ZusserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusz.adamus.zusservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZusserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
